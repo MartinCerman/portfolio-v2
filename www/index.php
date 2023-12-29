443e007ad322187c94aaca29aa998fe0bbdfc193
MartinCerman <cermanm@gmail.com>
2023-12-29 20:46:58 +0100

    Initial basic setup

---
+++ www/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="cs">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Moje osobní stránka která obsahuje portfolio, tech stack co používám, odkazy na GitHub a další užitečné věci.">
+    <meta name="keywords" content="HTML,CSS,JavaScript,portfolio,dotnet,csharp">
+    <meta name="author" content="Martin Čerman">
+    <title>MartinCerman.eu</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <script src="./assets/js/vendor/popper.min.js"></script>
+    <script src="./assets/js/vendor/bootstrap.min.js"></script>
+</body>
+
+</html>
