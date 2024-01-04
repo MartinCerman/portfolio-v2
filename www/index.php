2b2cdd3a1a02b994075cc3caf90487005fb4215c
MartinCerman <137774131+MartinCerman@users.noreply.github.com>
2024-01-04 17:40:28 +0100

    Hero section (#4)
    * Add hero section - first iteration
    
    * Add accordion into aboutMe section

--- www/index.php
+++ www/index.php
@@ -23,7 +23,7 @@
             <div class="collapse navbar-collapse" id="headerNav">
                 <ul class="navbar-nav w-sm-75 ms-sm-auto ms-sm-5 d-flex justify-content-around text-center">
                     <li class="nav-item ms-lg-5">
-                        <a class="nav-link" aria-current="page" href="#">About</a>
+                        <a class="nav-link" aria-current="page" href="#aboutMe">About</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="#">Skills</a>
@@ -54,8 +54,26 @@
     </nav>
     <main>
         <!-- About -->
-        <section>
-
+        <section id="aboutMe" class="my-5">
+            <div class="w-sm-75 pe-5">
+                <p class="text-secondary mb-0">Hello, I'm</p>
+                <h2 class="h1 fw-light mb-0">Martin Čerman</h2>
+                <p class="lead text-info">Junior Back-End Developer</p>
+                <p class="pe-5 lead">I am a junior programmer and I am currently studying a course focused on web applications and object-oriented programming at the Technical University of Ostrava.</p>
+            </div>
+            <div class="accordion" id="aboutMeAccordion">
+                <div class="w-100 text-center">
+                    <svg class="bi text-info border border-1 border-info rounded-5" width="60" height="30" fill="currentColor" data-bs-toggle="collapse" data-bs-target="#itemOne" aria-expanded="false" aria-controls="collapseOne">
+                        <use xlink:href="./assets/images/icons/bootstrap-icons.svg#chevron-double-down" />
+                    </svg>
+                </div>
+                <div id="itemOne" class="collapse">
+                    <div class="d-flex flex-column flex-md-row align-items-center">
+                        <img class="w-50 ms-1" style="max-width: 326px;" src="./assets/images/Robot-flying.png" alt="Robot">
+                        <p class="w-sm-75 lead">Je mi něco přes 30 let a jsem z Vysočiny. Vždy mě zajímalo jak se tvoří hry a k programování jsem se dostal právě od pokusů v herním enginu Unreal Engine 5, který je postavený na C++. Nakonec právě programování v C++ mě velmi rychle začlo bavíc více než vývoj her samotných a po pár měsících samostudia jsem získal možnost zúčastnit se kurzu <a class="link-primary" target="_blank" href="http://kurzy.vsb.cz/rekval-kurzy/programovani-obsah.php">OOP a programování www aplikací</a> a databází pořádaným VŠB - Technická univerzita Ostrava, kterým právě procházím.</p>
+                    </div>
+                </div>
+            </div>
         </section>
         <!-- /About -->
         <!-- Skills -->
