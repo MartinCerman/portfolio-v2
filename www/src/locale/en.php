25e956fdff59be7a2fea24b8909d1c0a737b5e65
MartinCerman <cermanm@gmail.com>
2024-01-08 19:52:27 +0100

    Add src/locale/en.php with english texts

---
+++ www/src/locale/en.php
@@ -0,0 +1,24 @@
+<?php
+return [
+    "header-about" => "About",
+    "header-skills" => "Skills",
+    "header-projects" => "Projects",
+    "header-contact" => "Contact",
+    "about-hello" => "Hello, I'm",
+    "about-title" => "Junior Back-End Developer",
+    "about-p1" => "I am a junior programmer and I am currently studying a course focused on web applications and object-oriented programming at the Technical University of Ostrava.",
+    "about-p2" => "Je mi něco přes 30 let a jsem z Vysočiny. Vždy mě zajímalo jak se tvoří hry a k programování jsem se dostal právě od pokusů v herním enginu Unreal Engine 5, který je postavený na C++. Nakonec právě programování v C++ mě velmi rychle začlo bavíc více než vývoj her samotných a po pár měsících samostudia jsem získal možnost zúčastnit se kurzu <a class='link-primary' target='_blank' href='http://kurzy.vsb.cz/rekval-kurzy/programovani-obsah.php'>OOP a programování www aplikací</a> a databází pořádaným VŠB - Technická univerzita Ostrava, kterým právě procházím.",
+    "skills-heading" => "Skills",
+    "skills-primary" => "Primary",
+    "skills-primary-text" => "- Technologies I use the most and study further.",
+    "skills-secondary" => "Secondary",
+    "skills-secondary-text" => "- Technologies I possess basic knowledge in and use occasionally.",
+    "skills-other" => "Other",
+    "skills-other-text" => "- At least I know what you're talking about, if that counts.",
+    "projects-heading" => "Projects",
+    "contact-heading" => "Contact Me",
+    "contact-name" => "Name:",
+    "contact-email" => "E-mail:",
+    "contact-message" => "Message:",
+    "contact-submit" => "Submit"
+];
