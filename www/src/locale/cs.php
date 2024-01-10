0f90536fcc789b09f5eda3dca03f4843c14729a5
MartinCerman <cermanm@gmail.com>
2024-01-10 21:01:35 +0100

    fixed a typo in cs.php

--- www/src/locale/cs.php
+++ www/src/locale/cs.php
@@ -9,7 +9,7 @@ return [
     "about-button" => "Více o mně",
     "about-title" => "Junior Back-End vývojář",
     "about-p1" => "Jsem junior programátor a právě studuji kurz zaměřený na webové aplikace a objektově orientované programování na VŠB - Technická univerzita Ostrava.",
-    "about-p2" => "Je mi něco přes 30 let a jsem z Vysočiny. Vždy mě zajímalo jak se tvoří hry a k programování jsem se dostal právě od pokusů v herním enginu Unreal Engine 5, který je postavený na C++. Nakonec právě programování v C++ mě velmi rychle začlo bavíc více než vývoj her samotných a po pár měsících samostudia jsem získal možnost zúčastnit se kurzu <a class='link-primary' target='_blank' href='http://kurzy.vsb.cz/rekval-kurzy/programovani-obsah.php'>OOP a programování www aplikací</a> a databází pořádaným VŠB - Technická univerzita Ostrava, kterým právě procházím.",
+    "about-p2" => "Je mi něco přes 30 let a jsem z Vysočiny. Vždy mě zajímalo jak se tvoří hry a k programování jsem se dostal právě od pokusů v herním enginu Unreal Engine 5, který je postavený na C++. Nakonec právě programování v C++ mě velmi rychle začlo bavit více než vývoj her samotných a po pár měsících samostudia jsem získal možnost zúčastnit se kurzu <a class='link-primary' target='_blank' href='http://kurzy.vsb.cz/rekval-kurzy/programovani-obsah.php'>OOP a programování www aplikací</a> a databází pořádaným VŠB - Technická univerzita Ostrava, kterým právě procházím.",
     "skills-heading" => "Dovednosti",
     "skills-primary" => "Primární",
     "skills-primary-text" => "- Technologie které používám nejvíce a dále se v nich rozvíjím.",
